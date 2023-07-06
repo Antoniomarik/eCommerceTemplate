b2de9f289588e7457a95c1babf75e9fb1d44344a
Antoniomarik <antonio.marik1996@gmail.com>
2023-07-06 23:52:54 +0200

    Initial commit

---
+++ src/main/java/com/ecommerce/springbotecommerce/SpringbotEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.springbotecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbotEcommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbotEcommerceApplication.class, args);
+    }
+
+}
