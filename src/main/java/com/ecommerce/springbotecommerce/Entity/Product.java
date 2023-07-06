82bb7a242bb90610e44b944fabbccdbf6f2144bc
Antoniomarik <antonio.marik1996@gmail.com>
2023-07-07 00:17:51 +0200

    created entities and configured app.properties

---
+++ src/main/java/com/ecommerce/springbotecommerce/Entity/Product.java
@@ -0,0 +1,55 @@
+package com.ecommerce.springbotecommerce.Entity;
+
+import com.ecommerce.springbotecommerce.ProductCategory;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name = "product")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "sku")
+    private String sku;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "unist_price")
+    private BigDecimal unitPrice;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @Column(name = "active")
+    private boolean active;
+
+    @Column(name = "units_in_stock")
+    private Integer unitsInStock;
+
+    @Column(name = "date_created")
+    @CreationTimestamp
+    private Date dateCreated;
+
+
+    @Column(name = "last_updated")
+    @UpdateTimestamp
+    private Date lastUpdated;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id",nullable = false)
+    private ProductCategory category;
+
+}
