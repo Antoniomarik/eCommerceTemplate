b9e42f91cd0c95b375a918bdd6fdade747f9e773
Antoniomarik <antonio.marik1996@gmail.com>
2023-07-08 01:25:23 +0200

    added CORS to ProductsRepository

--- src/main/java/com/ecommerce/springbotecommerce/dao/ProductRepository.java
+++ src/main/java/com/ecommerce/springbotecommerce/dao/ProductRepository.java
@@ -2,6 +2,8 @@ package com.ecommerce.springbotecommerce.dao;
 
 import com.ecommerce.springbotecommerce.Entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 public interface ProductRepository extends JpaRepository<Product,Long> {
 }
