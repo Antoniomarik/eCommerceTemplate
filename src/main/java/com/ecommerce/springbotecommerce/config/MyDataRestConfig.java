8b6cafd9c10535359ebc1928bec0a8155ccacbef
Antoniomarik <antonio.marik1996@gmail.com>
2023-07-11 19:50:45 +0200

    added new method to expose entity Ids

--- src/main/java/com/ecommerce/springbotecommerce/config/MyDataRestConfig.java
+++ src/main/java/com/ecommerce/springbotecommerce/config/MyDataRestConfig.java
@@ -2,19 +2,34 @@ package com.ecommerce.springbotecommerce.config;
 
 import com.ecommerce.springbotecommerce.Entity.Product;
 import com.ecommerce.springbotecommerce.ProductCategory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.metamodel.EntityType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    EntityManager entityManager;
+
+    @Autowired
+    public MyDataRestConfig(EntityManager theEntityManager){
+        entityManager = theEntityManager;
+    }
+
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
         RepositoryRestConfigurer.super.configureRepositoryRestConfiguration(config, cors);
 
-        HttpMethod[] unsupportedMethods = {HttpMethod.PUT,HttpMethod.DELETE,HttpMethod.POST};
+        HttpMethod[] unsupportedMethods = {HttpMethod.PUT, HttpMethod.DELETE, HttpMethod.POST};
 
         //Product
         config.getExposureConfiguration()
@@ -27,5 +42,24 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
                 .forDomainType(ProductCategory.class)
                 .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(unsupportedMethods)))
                 .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedMethods));
+
+        //call an intenral helpet method to expose ids;
+        exposeIds(config);
+    }
+       private void exposeIds (RepositoryRestConfiguration config) {
+
+           // get list of all entities
+            Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+
+            //create array of entities
+            List<Class> entityClasses = new ArrayList<>();
+
+            for(EntityType tempentityType : entities){
+                entityClasses.add(tempentityType.getJavaType());
+
+                //expose entities
+                Class[] domainTypes = entityClasses.toArray(new Class[0]);
+                config.exposeIdsFor(domainTypes);
+        }
     }
 }
