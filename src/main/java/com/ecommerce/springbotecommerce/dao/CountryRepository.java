26bc111c40b35dd882a95321afa30c76f4f61b97
Antoniomarik <antonio.marik1996@gmail.com>
2023-07-30 11:26:48 +0200

    added annotation jsonignore to country, and created new repos for entities

---
+++ src/main/java/com/ecommerce/springbotecommerce/dao/CountryRepository.java
@@ -0,0 +1,11 @@
+package com.ecommerce.springbotecommerce.dao;
+
+import com.ecommerce.springbotecommerce.Entity.Country;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin
+@RepositoryRestResource(collectionResourceRel = "countries", path = "countries")
+public interface CountryRepository extends JpaRepository<Country, Integer> {
+}
